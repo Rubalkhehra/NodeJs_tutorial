3838707b47c1f6a8a9a5954afcf0eb764cf842b7
Rubalkhehra <khehrarubal16@gmail.com>
2024-02-19 19:01:40 -0500

    reuploading files

--- demo.js
+++
@@ -1,6 +0,0 @@
-console.log("welcome");
-setTimeout(myfxn,3000);
-
-function myfxn(){
-    console.log("Rubal Khehra");
-}
--- demo2.js
+++
@@ -1,8 +0,0 @@
-async function getData(){
-    return "Rubal";
-}
-const data=getData();
-
-console.log(data)
-
-// always return a promise 
