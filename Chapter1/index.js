555494e3f2f94e7725e8aba877258148cab9167d
Rubalkhehra <khehrarubal16@gmail.com>
2024-02-24 23:57:54 -0500

    modules

--- Chapter1/index.js
+++ Chapter1/index.js
@@ -1,2 +1,9 @@
 // common js
 const fs=require("fs")
+
+fs.writeFileSync('demo.txt',)
+fs.readFileSync('demo.txt','utf-8')
+
+fs.readFile('demo.txt','utf-8',(err,data)=>{
+    console.log(data);
+})
