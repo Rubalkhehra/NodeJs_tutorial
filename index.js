6c23a82aab8fa68ca831b5d94c39e4c1d13cd456
Rubalkhehra <khehrarubal16@gmail.com>
2024-02-25 08:03:47 -0500

    ignore the node_modules package

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("this is outer module");
+const express=require('express');
+
+console.log("this is outer module, now server is sunning");
+
+const server=express();
+server.listen(8080);
